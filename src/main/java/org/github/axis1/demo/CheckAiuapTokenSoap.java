e7552d20db4b4059b4a8fb59ff5b00939ab14b22
hlfan3 <hlfan3@iflytek.com>
2022-12-01 11:01:19 +0800

    fix:增加token校验的接口

--- src/main/java/org/github/axis1/demo/CheckAiuapTokenSoap.java
+++ src/main/java/org/github/axis1/demo/CheckAiuapTokenSoap.java
@@ -33,8 +33,8 @@ public class CheckAiuapTokenSoap {
         LoginResponseBody loginResponseBody = LoginResponseBody
                 .builder()
                 .rsp("0")
-                .mainAcctId("abc")
-                .appAcctId("abcd")
+                .mainAcctId("administrator")
+                .appAcctId("administrator")
                 .build();
 
         LoginResponseInfo loginResponseInfo = LoginResponseInfo
--- src/main/java/org/github/axis1/ws/AutoConfigAxis1.java
+++ src/main/java/org/github/axis1/ws/AutoConfigAxis1.java
@@ -14,21 +14,21 @@ public class AutoConfigAxis1 {
 
 
 		ServletRegistrationBean<AxisServlet> servlet = new ServletRegistrationBean<AxisServlet>();
-		
+
 		servlet.setServlet(new AxisServlet());
-		servlet.addUrlMappings("/uap/webservice/*");
+		servlet.addUrlMappings("/uap/*");
 		servlet.setName("AxisServlet");
 		servlet.setLoadOnStartup(1);
-		
-		
+
+
 		//设置引擎
 		//与官方提供默认引擎区别:查找wsdd位置不同。一个是springboot位置，一个是WEB-INF位置(war)
 		System.setProperty(EngineConfigurationFactory.SYSTEM_PROPERTY_NAME,"org.github.axis1.ws.SpringBootEngineConfigurationFactoryServlet");
-		
+
 		return servlet;
-		
+
 	}
-	
-	
-	
+
+
+
 }
